4a028e92a5e383b86b8a218396a337c5bbfad289
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-19 23:26:42 -0300

    feat: add Event Error validation

--- internal/events/domain/event.go
+++ internal/events/domain/event.go
@@ -1,7 +1,16 @@
 package domain
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
+var (
+	ErrEventNameRequired = errors.New("Event name is required")
+	ErrEventDateFuture = errors.New("Event date must be in the future")
+	ErrEventCapacityZero = errors.New("Event capacity must be greater than zero (0)")
+	ErrEventPriceZero = errors.New("Event price must be greater than zero (0)")
+)
 type Rating string
 const (
 	RatingFree Rating = "L"
@@ -25,4 +34,24 @@ type Event struct {
 	PartnerID int
 	Spots []Spot
 	Tickets []Ticket
+}
+
+func (e Event) Validate() error {
+	if e.Name == "" {
+		return ErrEventNameRequired
+	}
+
+	if e.Date.Before(time.Now()) {
+		return ErrEventDateFuture
+	}
+
+	if e.Capacity <= 0 {
+		return ErrEventCapacityZero
+	}
+
+	if e.Price <= 0 {
+		return ErrEventPriceZero
+	}
+
+	return nil
 }
