334c95f08c0d4b67fdb4e800fb57fa4f8be2adf6
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-20 10:11:50 -0300

    feat: add first components

---
+++ src/app/components/EventCard.tsx
@@ -0,0 +1,28 @@
+import { EventModel } from '@/models'
+
+export type EventCardProps = {
+  event: EventModel
+}
+
+export default function EventCard({ event }: EventCardProps) {
+  const { id, name, organization, date, image_url, location, price, rating } =
+    event
+
+  const formattedDate = new Date(date).toLocaleDateString('pt-BR', {
+    weekday: 'long',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  })
+
+  return (
+    <div className="flex w-[277px] flex-col rounded-2xl bg-secondary">
+      <img src={image_url} alt={name} />
+      <div className="flex flex-col gap-y-2 px-4 py-6">
+        <p className="text-sm uppercase text-subtitle">{formattedDate}</p>
+        <p className="font-semibold">{name}</p>
+        <p className="text-sm font-normal">{location}</p>
+      </div>
+    </div>
+  )
+}
