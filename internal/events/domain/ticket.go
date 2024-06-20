93b0a578f12b0dbd97a30c4758ed32cf45ada92c
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-20 04:21:49 -0300

    feat: validate ticket

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,9 +1,15 @@
 package domain
 
+import "errors"
+
 type TicketType string
 const (
 	TicketTypeHalf TicketType = "half"
 	TicketTypeFull TicketType = "full"
+)
+
+var (
+	ErrTicketPriceZero = errors.New("Ticket price must be greater than zero (0)")
 )
 
 type Ticket struct {
@@ -12,4 +18,22 @@ type Ticket struct {
 	Spot *Spot
 	TicketType TicketType
 	Price float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+
+	return nil
 }
