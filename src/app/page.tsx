21af9643bc760f9780fb1df2db858b4d1be6039d
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-06-20 18:40:15 -0300

    feat: integrate api data into Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,16 @@ import { Title } from './components/Title'
 import EventCard from './components/EventCard'
 import { EventModel } from '@/models'
 
-export default function HomePage() {
-  const events: EventModel[] = [
-    {
-      id: '1',
-      name: 'Bring Me The Horizon',
-      organization: 'Organization 1',
-      date: '2024-12-01T00:00:00',
-      price: 200,
-      rating: '4.9',
-      image_url: 'https://unsplash.com/photos/9b9c5b9b0d1',
-      location: 'São Paulo, SP',
-    },
-    {
-      id: '2',
-      name: 'Falling In Reverse',
-      organization: 'Organization 2',
-      date: '2024-11-24T00:00:00',
-      price: 200,
-      rating: '4.8',
-      image_url: 'https://unsplash.com/photos/9b9c5b9b0d1',
-      location: 'São Paulo, SP',
-    },
-    {
-      id: '3',
-      name: 'Our Last Night',
-      organization: 'Organization 2',
-      date: '2024-11-21T00:00:00',
-      price: 200,
-      rating: '4.4',
-      image_url: 'https://unsplash.com/photos/9b9c5b9b0d1',
-      location: 'São Paulo, SP',
-    },
-  ]
+export async function getEvents(): Promise<EventModel[]> {
+  const response = await fetch('http://localhost:8080/events', {
+    cache: 'no-store',
+  })
+
+  return (await response.json()).events
+}
+
+export default async function HomePage() {
+  const events = await getEvents()
 
   return (
     <main className="mt-10 flex flex-col">
